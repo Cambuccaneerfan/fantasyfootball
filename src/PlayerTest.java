cfb9c1358f9f77a2a6491d37876d3aa7c2001aa7
gfgavin <gregfgavin@gmail.com>
2016-10-20 10:12:51 -0500

    Update PlayerTest.java

--- src/PlayerTest.java
+++ src/PlayerTest.java
@@ -1,23 +1,61 @@
+import java.util.Scanner;
+
 public class PlayerTest
 {
    public static void main (String[] args)
    {
-      Team team1 = new Team();
-      team1.setStartingQB(1);      
+      League theLeague = new League();
       
-      System.out.println("Free Agents");
+      Scanner keyboard = new Scanner(System.in);
       
-      int x = 0;
-      for (x = 0; x < PlayerList.playerList().size(); x++)
+      System.out.println("What is the name of the league?");
+      theLeague.setLeagueName(keyboard.nextLine());
+      
+      System.out.println("Who is managing Team 1?");
+      theLeague.getTeam1().setManagerName(keyboard.nextLine());
+      System.out.println("What is the name of " + theLeague.getTeam1().getManagerName() + "'s team?");
+      theLeague.getTeam1().setTeamName(keyboard.nextLine());
+      System.out.println("Who is managing Team 2?");
+      theLeague.getTeam2().setManagerName(keyboard.nextLine());
+      System.out.println("What is the name of " + theLeague.getTeam2().getManagerName() + "'s team?");
+      theLeague.getTeam2().setTeamName(keyboard.nextLine());
+      
+      System.out.println("");
+
+      System.out.println("All Available players:");
+      theLeague.getFreeAgents();
+      
+      System.out.println("");
+      
+      System.out.println("Which player did " + theLeague.getTeam1().getManagerName() + " draft? (enter number between 0 and 303)");
+      theLeague.getTeam1().setTeamPlayer1(keyboard.nextInt());
+      System.out.println("Which player did " + theLeague.getTeam1().getManagerName() + " draft? (enter number between 0 and 303)");
+      theLeague.getTeam1().setTeamPlayer2(keyboard.nextInt());
+      System.out.println("Which player did " + theLeague.getTeam2().getManagerName() + " draft? (enter number between 0 and 303)");
+      theLeague.getTeam2().setTeamPlayer1(keyboard.nextInt());
+      System.out.println("Which player did " + theLeague.getTeam2().getManagerName() + " draft? (enter number between 0 and 303)");
+      theLeague.getTeam2().setTeamPlayer2(keyboard.nextInt());
+	  
+      System.out.println("");
+      
+      System.out.println("Top 10 Free Agents After the Draft:");
+      theLeague.getFreeAgents(10);
+      
+      System.out.println("");
+      
+      System.out.println(theLeague.getLeagueName());
+      
+      System.out.println("Team 1");
+      System.out.println(theLeague.getTeam1().teamToString());
+      
+      System.out.println("");
+      
+      System.out.println("Team 2");
+      System.out.println(theLeague.getTeam2().teamToString());
+      
+      if(keyboard != null) 
       {
-         if (!team1.getPlayer(x).getIsOwned())
-         {
-            System.out.println(team1.getPlayer(x).playerToString());
-         }
-         
+    	  keyboard.close();
       }
-      
-      System.out.println("Owned");
-      System.out.println(team1.teamToString());
    }
 }
