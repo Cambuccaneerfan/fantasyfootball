2ec749fd8921052e42d28a9ba1250b13202cdec9
gfgavin <gregfgavin@gmail.com>
2016-10-20 10:10:04 -0500

    Update PlayerList.java

--- src/PlayerList.java
+++ src/PlayerList.java
@@ -1,6 +1,11 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class PlayerList
 {
-   public static final ArrayList<Player> playerList()
+   public static ArrayList<Player> playerList()
    {
       ArrayList<Player> playList = new ArrayList<Player>();
       
@@ -43,58 +48,3 @@ public class PlayerList
       return playList;
    }
 }
-   /*
-   public static Player player1;
-   public static Player player2;
-   public static Player player3;
-   public static Player player4;
-   public static Player player5;
-   
-   
-   
-   
-   
-   public static void main (String[] args)
-   {
-      player1 = new Player("WR", "Antonio", "Brown", "PIT", false);
-      player2 = new Player("RB", "David", "Johnson", "ARI", false);
-      player3 = new Player("WR", "Julio", "Jones", "ATL", false);
-      player4 = new Player("RB", "Todd", "Gurley", "LAR", false);
-      player5 = new Player("RB", "Ezekiel", "Elliot", "DAL", false);
-      
-      System.out.println(player1.playerToString());
-      System.out.println(player2.playerToString());
-      System.out.println(player3.playerToString());
-      System.out.println(player4.playerToString());
-      System.out.println(player5.playerToString());
-   }
-}
-   
-  
-   
-   public PlayerList()
-   {
-      player1 = new Player("WR", "Antonio", "Brown", "PIT", false);
-      player2 = new Player("RB", "David", "Johnson", "ARI", false);
-      player3 = new Player("WR", "Julio", "Jones", "ATL", false);
-      player4 = new Player("RB", "Todd", "Gurley", "LAR", false);
-      player5 = new Player("RB", "Ezekiel", "Elliot", "DAL", false);
-   }
-   
-   
-   public class PlayerList
-{
-   public static void main (String[] args)
-   {
-      Player player1 = new Player();
-      Player player2 = new Player();
-      
-      player1.setPosition(WR);
-      player1.setFirstName(Antonio);
-      player1.setLastName(Brown);
-      player1.setNflTeam(PIT);
-      
-      System.out.println(player1.playerToString());
-   }
-}
-   */
