f6efbec4d6e508120a1645a1b933456fd886ea73
gfgavin <gregfgavin@gmail.com>
2016-11-15 16:54:09 -0600

    fr1-7 and roster arraylist
    fr1-7 methods and attributes added, and players on team are now in arraylist of Players (roster)

--- src/Team.java
+++ src/Team.java
@@ -1,17 +1,40 @@
+import java.util.ArrayList;
+
 public class Team
 {
    private String managerName;
    private String teamName;
-	
+
+   private ArrayList<Player> roster;
    private Player teamPlayer1;
    private Player teamPlayer2;
+   
+   private static int numQB;
+   private static int numWR;
+   private static int numRB;
+   private static int numTE;
+   private static int numBench;
+   
+   // Commissioner should be able to create a league, and establish the settings for the league. 
+   // The commissioner must be able to set the league name, number of teams in the league,
+   // scoring rules, maximum players per team, starting players per team, the managers in the league, 
+   // and also be able to enter draft results.
    
    public Team()
    {
 	  managerName = "";
 	  teamName = "";
+	  
+	  roster = new ArrayList<Player>();
+	  
 	  teamPlayer1 = new Player();
       teamPlayer2 = new Player();
+      
+      numQB = 0;
+      numWR = 0;
+      numRB = 0;
+      numTE = 0;
+      numBench = 0;
    }
    
    public String getManagerName()
@@ -24,6 +47,11 @@ public class Team
       return teamName;
    }
    
+   public ArrayList<Player> getRoster()
+	{
+		return roster;
+	}
+   
    public Player getTeamPlayer1()
    {
       return teamPlayer1;
@@ -32,6 +60,36 @@ public class Team
    public Player getTeamPlayer2()
    {
       return teamPlayer2;
+   }
+   
+   public static int getNumQB()
+   {
+      return numQB;
+   }
+   
+   public static int getNumWR()
+   {
+      return numWR;
+   }
+   
+   public static int getNumRB()
+   {
+      return numRB;
+   }
+   
+   public static int getNumTE()
+   {
+      return numTE;
+   }
+   
+   public static int getNumBench()
+   {
+      return numBench;
+   }
+   
+   public static int getMaxPlayers()
+   {
+      return numQB + numWR + numRB + numTE + numBench;
    }
    
    public void setManagerName(String mName)
@@ -70,14 +128,50 @@ public class Team
       League.playerList().set(x, teamPlayer2);
    }
    
+   public void setTeamPlayer(int x, int y)
+   {
+      roster.set(x, League.playerList().get(y));
+      roster.get(x).setIsOwned();
+      League.playerList().set(y, roster.get(x));
+   }
+   
+   public static void setNumQB(int x)
+   {
+      numQB = x;
+   }
+   
+   public static void setNumWR(int x)
+   {
+      numWR = x;
+   }
+   
+   public static void setNumRB(int x)
+   {
+      numRB = x;
+   }
+   
+   public static void setNumTE(int x)
+   {
+      numTE = x;
+   }
+   
+   public static void setNumBench(int x)
+   {
+      numBench = x;
+   }
+   
    public Player getPlayer(int y)
    {
       return League.playerList().get(y);
    }
    
    
-   public String teamToString()
+   public void teamToString()
    {
-      return "Manager: " + managerName + "\n" + "Team: " + teamName + "\n" + teamPlayer1.playerToString() + "\n" + teamPlayer2.playerToString();
+	   System.out.println("Manager: " + managerName + "\n" + "Team: " + teamName);
+	   for (int x = 0; x < getMaxPlayers(); x++)
+  	   {
+		   System.out.println(roster.get(x).playerToString());
+  	   }
    }
 }
